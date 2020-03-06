b648311e80645c0bc5e79ccbf76c8039cb0678b0
husin14 <54317405+husin14@users.noreply.github.com>
2020-03-06 12:55:14 +0700

    Add files via upload
    PERTAMA

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author husin14
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+       
+        
+        for (int j = 0; j < 10; j++) {
+            System.out.print(j);
+            
+        }
+        
+        
+//        nilai nilaiakhir = new nilai();
+//        
+//        
+//        
+//        //NAMA
+//        Scanner inputnama = new Scanner(System.in);
+//        System.out.println("Nama = ");
+////        String nama = inputnama.next();
+//        nilaiakhir.namaku = inputnama.nextLine();
+//        //NIM
+//        Scanner inputnim = new Scanner(System.in);
+//        System.out.println("NIM = ");
+//        String nim = inputnim.next();
+//        //UTS
+//        Scanner inpututs = new Scanner(System.in);
+//        System.out.println("UTS = ");
+//        float uts = inpututs.nextFloat();
+//        //UAS
+//        Scanner inputuas = new Scanner(System.in);
+//        System.out.println("UAS = ");
+//        float uas = inputuas.nextFloat();
+//        //TUGAS
+//        Scanner inputtugas = new Scanner(System.in);
+//        System.out.println("TUGAS = ");
+//        float tugas = inputtugas.nextFloat();
+//        
+//        
+//        System.out.println("Nama anda = " + nilaiakhir.namaku);
+//        System.out.println("NIM = " + nim);
+//        System.out.println("Nilai TUGAS = " + tugas);
+//        System.out.println("Nilai UTS = " + uts);
+//        System.out.println("Nilai UAS = " + uas);
+//        
+//        float total = (float) ((tugas * 0.30) + (uts*0.40) + (uas * 0.40) );
+//        
+//        System.out.println("Nilai akhir anda adalah = "+total);
+//        
+//        if (total > 70) {
+//            System.out.println("TUNTASSS");
+//        }
+//        else {
+//            System.out.println("ULANGI LAGI");
+//        }
+//        
+//        
+        
+        
+//    Scanner inputnama = new Scanner(System.in);  // Create a Scanner object
+//    System.out.println("Masukan nama anda:");
+//
+//    String nama = inputnama.nextLine();  // Read user input
+//    System.out.println("Jadi nama anda adalah : " + nama);  // Output user input
+    
+    }
+    
+}
---
+++ JavaApplication1/src/javaapplication1/nilai.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author husin14
+ */
+public class nilai {
+    String namaku;
+    String nimku;
+    Float tugasku;
+    Float utsku;
+    Float uasku;
+    Float NilaiAkhirku;
+    
+}
